4e5796cd9b6d37669d77d1ddbb67275acfcb836b
caetasousa <caetasousa@gmail.com>
2024-04-25 07:48:12 -0300

    primeiro commit

---
+++ src/main/java/caeta/techalanger/TechalangerApplication.java
@@ -0,0 +1,13 @@
+package caeta.techalanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechalangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechalangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caeta/techalanger/TechalangerApplicationTests.java
@@ -0,0 +1,13 @@
+package caeta.techalanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechalangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
